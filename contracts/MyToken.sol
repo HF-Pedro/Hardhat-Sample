9138c8c81fa4cff3a043ff812812540328eab1f3
HF-Pedro <pedrohsxd@gmail.com>
2024-07-03 13:11:32 -0300

    teste

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -6,10 +6,24 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC20, ERC20Permit {
+contract MyToken is Ownable, ERC20, ERC20Permit {
+
+    error InvalidAddress(string message);
     
-    constructor(uint256 initialSupply) ERC20("MyToken", "MTK") ERC20Permit("MyToken") {
+    constructor(uint256 initialSupply) ERC20("MyToken", "MTK") ERC20Permit("MyToken") Ownable(msg.sender) {
         _mint(msg.sender, initialSupply * (10 ** 18));
     }
 
+    function mint(address recipient, uint256 value) external onlyOwner{
+        _checkAddress(recipient);
+        _mint(recipient, value);
+    }
+
+    function _checkAddress(address recipient) public pure{
+        require(recipient == address(0), "No zero address allowed" );
+        // substituir por if e custom error
+        // substituir por require com custom error
+        // elencar da mais barata a mais cara, trazer o custo de transação de cada uma
+    }
+    
 }
